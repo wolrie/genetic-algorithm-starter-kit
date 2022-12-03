d43d71f33e510f755b728cc8a51b8546cbca44ff
wolrie <107036127+wolrie@users.noreply.github.com>
2022-12-03 16:18:59 +0100

    refactor: change function names and improve readability

--- sudoku_solver/utils/solve.py
+++ sudoku_solver/utils/solve.py
@@ -4,17 +4,17 @@ from typing import List
 from sudoku_solver.etc import config
 
 
-def check_row_update(board: List[List[int]], row: int, digit: int) -> bool:
+def _is_valid_row_update(board: List[List[int]], row: int, digit: int) -> bool:
     """Check for duplicate entry in row."""
     return not any([board[row][i] == digit for i in range(9)])
 
 
-def check_col_update(board: List[List[int]], col: int, digit: int) -> bool:
+def _is_valid_col_update(board: List[List[int]], col: int, digit: int) -> bool:
     """Check for duplicate entry in column."""
     return not any([board[i][col] == digit for i in range(9)])
 
 
-def check_box_update(board: List[List[int]], row: int, col: int, digit: int) -> bool:
+def _is_valid_box_update(board: List[List[int]], row: int, col: int, digit: int) -> bool:
     """Check for duplicate entry in box."""
     row, col = 3 * (row // 3), 3 * (col // 3)
     return not any([board[row + i][col + j] == digit for i, j in product(range(3), repeat=2)])
@@ -30,7 +30,10 @@ def get_empty_cell_coordinates(board: List[List[int]]) -> List[int]:
 
 def is_valid_update(board: List[List[int]], row: int, col: int, digit: int) -> bool:
     """Check whether given cell entry at given coordinates is a valid update."""
-    flag = (check_row_update(board, row=row, digit=digit) and
-            check_col_update(board, col=col, digit=digit) and
-            check_box_update(board, row=row, col=col, digit=digit))
-    return True if flag else False
+    return all(
+        [
+            _is_valid_row_update(board, row=row, digit=digit),
+            _is_valid_col_update(board, col=col, digit=digit),
+            _is_valid_box_update(board, row=row, col=col, digit=digit),
+        ]
+    )
